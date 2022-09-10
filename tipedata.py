1427992d862ea3d11e857b6fb75df5e06a3a432c
Sigma <109166519+Tyo009@users.noreply.github.com>
2022-09-10 10:29:57 +0700

    Add files via upload

---
+++ tipedata.py
@@ -0,0 +1,32 @@
+# Tipe data integer (Bilangan Bulat)
+nama ="Sigma Guntur"
+umur =18
+ukuransepatu = 42,5
+
+print ("Nama Saya Adalah" + nama + "Umur saya" + str(umur))
+# tipe data float (Angka Numerik)
+print ("Nama Saya Adalah" + nama + "Umur saya" + str(umur))
+print("Umur saya Adalah : ", umur)
+print("Ukuran Sepatu ", ukuransepatu )   
+# boolean (Salah Benar)
+
+# Tipe List
+
+hewan = ["Ayam" , "Sapi" , "Kucing"]
+print(hewan [1])
+print(hewan[0])
+print(hewan [2])
+
+# Tipe Numeric Tambah
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print(" penjumlahan : " + str (angka1) , str (angka2) + str (angka3)) 
+print(" perkalian : " + str (angka1) * str (angka2) * str (angka3)) 
+
+
+
+
+
+# tugasnya menghitung konversi suhu terserah
+# Menghtung luas bangun ruang
